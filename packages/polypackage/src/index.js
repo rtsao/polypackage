fbec071ab4adeb1d035821c9c2a07d6976debac9
Ryan Tsao <rtsao@uber.com>
2016-09-01 15:07:31 -0700

    eh

--- packages/polypackage/src/index.js
+++ packages/polypackage/src/index.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const derivePkg = require('derive-pkg-core');
+const extend = require('just-extend');
 
 function polyPackage({basedir, srcDirname, outputDirname, transpileDirname}) {
   derivePkg({
     srcDir: basedir,
     destDir: path.join(basedir, outputDirname),
     transformFn: (pkg) => {
-      return Object.assign({}, pkg, {
+      return extend({}, pkg, {
         main: path.join(transpileDirname, 'index.js'),
         module: 'es/index.js'
       });
